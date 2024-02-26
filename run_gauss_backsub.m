bc9edd4b650a2881883f425402292d6b2aec9645
richmarc5 <113929118+richmarc5@users.noreply.github.com>
2024-02-26 02:35:49 +0000

    Add files via upload

--- run_gauss_backsub.m
+++ run_gauss_backsub.m
@@ -1,15 +1,33 @@
 clear all;
 close all;
 
-n=5;
-A=10*rand(n,n);
-b=10*rand(1,n)';
+n=100;
+A=41*rand(n,n);
+b=12*rand(1,n)';
+tic
 xsol = linsolve(A,b);
+toc
+tic
+
+##[Anew,bnew] = MyGaussFactorise(A,b);
+##toc
+##tic
+##[xfinal] = BacksubRC(Anew,bnew);
+##toc
+##figure;
+##plot(1:n,xsol,'--+',1:n,xfinal,'o')
+fprintf('Lu decomp \n')
+tic
+%[L,U]=lu(A);
+[L,U,P] = lu(A);
 
 
 
-[Anew,bnew] = MyGaussFactorise(A,b);
-[xfinal] = BacksubRC(Anew,bnew);
+[y] =ForwardsubRC(L,P*b);
+[xf] = BacksubRC(U,y);
+toc
+
 
 figure;
-plot(1:n,xsol,'--+',1:n,xfinal,'o')
+plot(1:n,xsol,'--+',1:n,xf,'o')
+title('LU version')
